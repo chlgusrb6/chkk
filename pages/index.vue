54eec2ab4f6d232abfe55169b42518c320c6f8de
hyunkyuchoi <chlgusrb6@gmail.com>
2018-05-15 11:07:00 +0900

    [2018-05-15] DogLove #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section">
+  <section class="hero is-medium is-primary is-bold">
     <div class="hero is-primary">
       <div class="hero-body">
         <div class="container">
@@ -13,6 +13,21 @@
           <div class="message-header">개란 무엇인가?</div>
           <div class="message-body">개(학명 : class inpus)</div>
         </article>
+        <div class="columns"> 
+      <div class="column"> 
+         <div class="container"> 
+           <figure class="image is-squre"> 
+           <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg" alt=""> 
+           </figure> 
+         </div> 
+       </div> 
+       <div class="column"> 
+         <figure class="image is-squre"> 
+           <img src="" alt=""> 
+                    </figure> 
+ </div> 
+     </div> 
+
   </section>
 </template>
 
