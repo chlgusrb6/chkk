b3bc937f569daf0e4c8eede51e5aff87092cc2bc
hyunkyuchoi <chlgusrb6@gmail.com>
2018-05-15 11:33:15 +0900

    [2018-05-15] DogLove #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,46 @@
 <template>
-  <section class="hero is-medium is-primary is-bold">
-    <div class="hero is-primary">
+  <section class="section"> 
+   <div class="hero is-danger"> 
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">개사랑</h1>
+          <h1 class="title is-1">개사랑</h1>
           <h2 class="subtitle">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>
-        </div>
+       </div>
         </div>
         </div>
         <br>
         <article class ="message">
-          <div class="message-header">개란 무엇인가?</div>
-          <div class="message-body">개(학명 : class inpus)</div>
+          <div class="message-header">
+            <p>오늘의 개 사진</p></div>
+          <div class="message-body"></div>
         </article>
         <div class="columns"> 
       <div class="column"> 
          <div class="container"> 
-           <figure class="image is-squre"> 
-           <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg" alt=""> 
-           </figure> 
+            <figure class="image is-squre">  
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg" alt="">  
+          </figure> 
          </div> 
        </div> 
        <div class="column"> 
          <figure class="image is-squre"> 
-           <img src="" alt=""> 
-                    </figure> 
- </div> 
-     </div> 
-
-  </section>
+           <img v-bind:src="image" alt=""> 
+          </figure> 
+    </div> 
+  </div> 
+ </section>
 </template>
 
-<script>
 
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {};
+  },
+  async asyncData() {
+    const myImage = await axios.get("https://dog.ceo/api/breeds/image/random");
+    return { image: myImage.data.message };
+  }
+};
 </script>
