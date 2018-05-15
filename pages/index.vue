aa102e1410bbfe682f2852411ee0c1bccce9590a
hyunkyuchoi <chlgusrb6@gmail.com>
2018-05-15 11:00:34 +0900

    [2018-05-15] DogLove #1

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">개사랑</h1>
+          <h2 class="subtitle">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>
+        </div>
+        </div>
+        </div>
+        <br>
+        <article class ="message">
+          <div class="message-header">개란 무엇인가?</div>
+          <div class="message-body">개(학명 : class inpus)</div>
+        </article>
+  </section>
+</template>
+
+<script>
+
+</script>
